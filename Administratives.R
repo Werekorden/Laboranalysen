7c39c3818d87d460c74767e150a252b97655b128
Werekorden <werekorden@googlemail.com>
2021-09-05 18:07:58 +0200

    Administrative Datei für Password usw. wichtig

---
+++ Administratives.R
@@ -0,0 +1,9 @@
+
+
+#Wichtig um sich bei GitHub zu verfizieren
+install.packages("gitcreds")
+library(gitcreds)
+library(usethis) # wichtig für vers. laut homepage https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r/ 
+gitcreds_set()
+
+#dann token einkopieren
